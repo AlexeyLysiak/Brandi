0822ede7c32777f9cdc6e75d2d802075318f8067
Alexey Lysiak <lysiak.oleksii@gmail.com>
2020-05-21 22:28:57 +0300

    create & repair hero

---
+++ assets/js/swiper.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper('.swiper-container', {
+  direction: 'vertical',
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+
+var swiper = new Swiper('.cards-container', {
+pagination: {
+  el: '.swiper-pagination',
+},
+});
