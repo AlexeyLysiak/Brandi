cb906d02e98779e9e8045cbc4e11ed92a4e4dab4
Alexey Lysiak <lysiak.oleksii@gmail.com>
2020-05-23 14:44:51 +0300

    add slider

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,13 +1,37 @@
+// var swiper = new Swiper('.swiper-container', {
+//   direction: 'vertical',
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//   },
+// });
+
 var swiper = new Swiper('.swiper-container', {
-  direction: 'vertical',
+  slidesPerView: 3,
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
   },
-});
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 100,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 100
+    },
 
-var swiper = new Swiper('.cards-container', {
-pagination: {
-  el: '.swiper-pagination',
-},
+    960: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    }
+  },
 });
